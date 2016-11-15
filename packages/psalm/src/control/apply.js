599cb7c726608e3a580cb9c66f7fc45fb90b33e6
Syaiful Bahri <syaiful@pengunjungblog.com>
2016-11-15 07:08:45 +0700

    add apply

---
+++ packages/psalm/src/control/apply.js
@@ -0,0 +1,72 @@
+import { map } from '../data/functor'
+import * as fl from '../util/fantasy'
+import { unsoppertedMethod } from '../util/error'
+import { curryN, constant, id } from '../data/function'
+
+/**
+ * implementation for function
+ */
+const apFn = (a, b) =>
+  x => a(x)(b(x))
+
+/**
+ * implementation for array
+ */
+const apArray = (fs, xs) => fs.reduce((acc, f) => acc.concat(map(f, xs)), [])
+
+/**
+ * A value that implements the Apply specification must also implement the Functor
+ * specification:
+ *
+ * Laws:
+ * - v.ap(u.ap(a.map(f => g => x => f(g(x))))) === v.ap(u).ap(a)
+ */
+export const ap = curryN(2, (apF, apV) => {
+  return typeof apV[fl.ap] === 'function'         ? apV[fl.ap](apF)
+  :      typeof apF        === 'function'         ? apFn(apF, apV)
+  :      Array.isArray(apF) && Array.isArray(apV) ? apArray(apF, apV)
+  :                                                 unsoppertedMethod(fl.ap)(apV)
+})
+
+/**
+ * Combine two effectful actions, keeping only the result of the first.
+ *
+ * @sig apFirst :: forall a b f. Apply f => f a -> f b -> f a
+ */
+export const apFirst = curryN(2, (a, b) => ap(map(constant, a), b))
+
+/**
+ * Combine two effectful actions, keeping only the result of the second
+ *
+ * @sig apSecond :: forall a b f. Apply f => f a -> f b -> f b
+ */
+export const apSecond = curryN(2, (a, b) => ap(map(constant(id), a), b))
+
+/**
+ * Lift a function of two arguments to a function which accepts and returns
+ * values wrapped with the type constructor `f`.
+ *
+ * @sig lift2 :: forall a b c f. Apply f => (a -> b -> c) -> f a -> f b -> f c
+ */
+export const lift2 = curryN(3, (f, a, b) => ap(map(f, a), b))
+
+/**
+ * Lift a function of three arguments to a function which accepts and returns
+ * values wrapped with the type constructor `f`.
+ *
+ * @sig lift3 :: forall a b c d f. Apply f => (a -> b -> c -> d) -> f a -> f b -> f c -> f d
+ */
+export const lift3 = curryN(4, (f, a, b, c) => ap(lift2(f, a, b), c))
+
+/**
+ * Lift a function of four arguments to a function which accepts and returns
+ * values wrapped with the type constructor `f`.
+ *
+ * @sig lift4 :: forall a b c d e f. Apply => (a -> b -> c -> d -> e) -> f a -> f b -> f c -> f d -> f e
+ */
+export const lift4 = curryN(5, (f, a, b, c, d) => ap(lift3(f, a, b, c), d))
+
+/**
+ * @sig lift5 :: forall a b c d e f g. Apply f => (a -> b -> c -> d -> e -> g) -> f a -> f b-> f c -> f d -> f e -> f g
+ */
+export const lift5 = curryN(6, (f, a, b, c, d, e) => ap(lift4(f, a, b, c, d), e))
---
+++ packages/psalm/src/data/ord.js
@@ -0,0 +1,125 @@
+import { curryN, on } from './function'
+import { unsoppertedMethod } from '../util/error'
+import { LT, GT, EQ, unsafeCompare } from './ordering'
+
+/**
+ * A value that implement Ord must also implement fantasy land Setoid interface.
+ * Ord (Ordering) represents values which support comparisons with a total order:
+ *
+ * `Ord` instances should satisfy the laws of total orderings:
+ * - Reflexivity: `a <= a`
+ * - Antisymmetry: if `a <= b` and `b <= a` then `a = b`
+ * - Transitivity: if `a <= b` and `b <= c` then `a <= c`
+ */
+const compareMethod = 'fl-contrib/compare'
+
+/**
+ * Dispatch Ord specification to the implementation.
+ *
+ * compare :: forall a. Ord a => a -> a -> Ordering
+ */
+export const compare = curryN(2, (a, b) => {
+  return typeof a[compareMethod] === 'function' ? a[compareMethod](b)
+  :      typeof a === 'string'                  ? unsafeCompare(a, b)
+  :      typeof a === 'number'                  ? unsafeCompare(a, b)
+  :      typeof a === 'boolean'                 ? unsafeCompare(a, b)
+  :      Array.isArray(a)                       ? compareArray(a, b)
+  :                                               unsoppertedMethod(compareMethod)(a)
+})
+
+/**
+ *
+ */
+export const gt = curryN(2, (a, b) => {
+  return compare(a, b).matchWith({
+    LT: () => false,
+    GT: () => true,
+    EQ: () => false
+  })
+})
+
+export const lt = curryN(2, (a, b) => {
+  return compare(a, b).matchWith({
+    LT: () => true,
+    GT: () => false,
+    EQ: () => false
+  })
+})
+
+export const gte = curryN(2, (a, b) => {
+  return compare(a, b).matchWith({
+    LT: () => false,
+    GT: () => true,
+    EQ: () => true
+  })
+})
+
+export const lte = curryN(2, (a, b) => {
+  return compare(a, b).matchWith({
+    LT: () => true,
+    GT: () => false,
+    EQ: () => true
+  })
+})
+
+/**
+ * Compares two values by mapping them to a type with an `Ord` instance.
+ *
+ * @sig comparing :: forall a b. Ord b => (a -> b) -> (a -> a -> Ordering)
+ */
+export const comparing = on(compare)
+
+/**
+ * Take the minimum of two values. If they are considered equal, the first
+ * argument is choosen.
+ *
+ * @sig min :: forall a. Ord a => a -> a -> a
+ */
+export const min = curry(2, (x, y) => {
+  return compare(x, y).matchWith({
+    LT: () => x,
+    GT: () => y,
+    EQ: () => x
+  })
+})
+
+/**
+ * Take the maximum of two values. If they are considered equal, the first
+ * argument is choosen.
+ *
+ * @sig max :: forall a. Ord a => a -> a -> a
+ */
+export const max = curry(2, (x, y) => {
+  return compare(x, y).matchWith({
+    LT: () => y,
+    GT: () => x,
+    EQ: () => x
+  })
+})
+
+/**
+ * Clamp a value between a minimum and a maximum. For example:
+ * ```javascript
+ * let f = clamp(0, 10)
+ * f(-2) // 0
+ * f(11) // 10
+ * f(5)  // 5
+ * ```
+ */
+export const clamp = curry(3, (low, hi, x) => min(hi, max(low, x)))
+
+function compareArray(xs, ys) {
+  let i = 0
+  const xlen = xs.length
+  const ylen = ys.length
+  while (i < xlen && i < ylen) {
+    const result = compare(xs[i], ys[i])
+    if (!EQ.hasInstance(result)) {
+      return result
+    }
+    i++
+  }
+  return xlen === ylen ? EQ()
+  :      xlen >   ylen ? GT()
+  :                      LT()
+}
