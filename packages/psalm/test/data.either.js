6e7a5d1637a0db25789256b26c9207b8deb27b28
Syaiful Bahri <sbahri@indo-soft.com>
2016-11-21 16:12:51 +0700

    (test Either) add either function test

--- packages/psalm/test/data.either.js
+++ packages/psalm/test/data.either.js
@@ -1,11 +1,23 @@
 import { expect } from 'chai'
 import { property } from 'jsverify'
 
+import { either } from '../lib/data/either/extra'
 import { Left, Right } from '../lib/data/either/core'
+import { equals } from '../lib/data/setoid'
 import * as laws from './helpers/laws'
 
 
 describe('Either', () => {
+  describe('either', () => {
+    property('apply to first function if Either is Left',
+             'nat -> nat', 'nat -> nat', 'nat',
+             (f, g, x) => equals(either(f, g, Left(x)), f(x)))
+
+    property('apply to second function if Either is Right',
+             'nat -> nat', 'nat -> nat', 'nat',
+             (f, g, x) => equals(either(f, g, Right(x)), g(x)))
+  })
+
   describe('Fantasy Land', () => {
     laws.Setoid(Left)
     laws.Setoid(Right)
