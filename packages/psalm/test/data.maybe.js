927adcd1cf0bdcca4367423f6fdfd0fc99fe4b29
Syaiful Bahri <sbahri@indo-soft.com>
2016-11-22 17:27:55 +0700

    add test for stack safe traverse

--- packages/psalm/test/data.maybe.js
+++ packages/psalm/test/data.maybe.js
@@ -2,7 +2,9 @@ import { expect } from 'chai'
 import { property } from 'jsverify'
 
 import { Nothing, Just } from '../lib/data/maybe/core'
-import { maybe, maybe_ } from '../lib/data/maybe/extra'
+import {
+  maybe, maybe_, fromMaybe, isJust, isNothing
+} from '../lib/data/maybe/extra'
 import { equals } from '../lib/data/setoid'
 import * as laws from './helpers/laws'
 
@@ -26,6 +28,36 @@ describe('Maybe', () => {
       expect(count).to.be.equal(0)
       expect(maybe_(increment, Math.sqrt, Nothing.value)).to.not.equal(3)
       expect(count).to.be.equal(1)
+    })
+  })
+
+  describe('fromMaybe', () => {
+    property('extract value inside Just if argument 2 is Just',
+             'nat', 'nat',
+             (a, b) => equals(fromMaybe(a, Just(b)), b))
+
+    property('return default value if argument 2 is Nothing',
+             'nat',
+             a => equals(fromMaybe(a, Nothing.value), a))
+  })
+
+  describe('isJust', () => {
+    it('return true if applied Just', () => {
+      expect(isJust(Just('foo'))).to.be.equal(true)
+    })
+
+    it('return false if applied Right', () => {
+      expect(isJust(Nothing.value)).to.be.equal(false)
+    })
+  })
+
+  describe('isNothing', () => {
+    it('return true if applied Nothing', () => {
+      expect(isNothing(Nothing.value)).to.be.equal(true)
+    })
+
+    it('return false if applied Just', () => {
+      expect(isNothing(Just('baz'))).to.be.equal(false)
     })
   })
 
--- packages/psalm/test/data.traversable.js
+++ packages/psalm/test/data.traversable.js
@@ -1,6 +1,11 @@
 import { expect } from 'chai'
+import { property } from 'jsverify'
 
+import { pure } from '../lib/control/applicative'
+import { map } from '../lib/data/functor'
 import { flip } from '../lib/data/function'
+import { Maybe, Nothing, Just } from '../lib/data/maybe'
+import { equals } from '../lib/data/setoid'
 import * as T from '../lib/data/traversable'
 
 
@@ -25,6 +30,49 @@ export const MAX_STACK = computeCallStack()
 describe('Traversable', () => {
   const add = (x, y) => x + y
   const sub = (x, y) => x - y
+
+  describe('traverse', () => {
+    property('simple traverse operation', 'nat', 'nat', 'nat', (a, b, c) =>
+      equals(
+        T.traverse(
+          pure(Maybe),
+          x => Just(add(10, x)),
+          [a, b, c]
+        ),
+        Just(map(x => add(10, x), [a, b, c]))
+      )
+    )
+
+    property('operates on a list of applicatives', 'nat', 'nat', 'nat', (a, b, c) =>
+      equals(
+        T.traverse(
+          pure(Maybe),
+          map(x => add(10, x)),
+          [Just(a), Just(b), Just(c)]
+        ),
+        Just(map(x => add(10, x), [a, b, c]))
+      )
+    )
+
+    it('stack safe for array implementation', () => {
+      const run = () =>
+        T.traverse(pure(Maybe), x => Just(add(10, x)), arrayFrom1UpTo(MAX_STACK))
+
+      expect(run).to.not.throw(/Maximum call stack/)
+    })
+  })
+
+  describe('sequence', () => {
+    property('operates on a list of applicatives', 'nat', 'nat', 'nat', (a, b, c) =>
+      equals(
+        T.sequence(pure(Maybe), [Just(a), Just(b), Just(c)]),
+        Just([a, b, c])
+      ) && equals(
+        T.sequence(pure(Maybe), [Just(a), Just(b), Just(c), Nothing.value]),
+        Nothing.value
+      )
+    )
+  })
 
   it('scanl accumulate intermediate from right', () => {
     const a = T.scanl(sub, 10, [1, 2, 3])
